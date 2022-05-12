419bf59474fc59048a41dc80ad156112dc432270
Artyom Nazarov <artyom.nazaroff@yandex.ru>
2022-05-12 14:18:26 +0400

    initial commit

---
+++ src/components/Component.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Component.module.scss";
+
+const Component = () => {
+  return <div className={styles.test}>Hello</div>;
+};
+
+export default Component;
